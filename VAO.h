9c43b3f09552f3470ec3c51b6c33891a0f35f8b5
Sword <86320186+Swordzi@users.noreply.github.com>
2021-10-26 15:44:59 +0300

    Main stuff
    Pää pasklaa

---
+++ VAO.h
@@ -0,0 +1,27 @@
+#ifndef VAO_CLASS_H
+#define VAO_CLASS_H
+
+#include<glad/glad.h>
+#include"VBO.h"
+
+class VAO
+{
+public:
+	// ID reference for the Vertex Array Object
+	GLuint ID;
+	// Constructor that generates a VAO ID
+	VAO();
+
+	void LinkVBO(VBO& VBO, GLuint layout);
+
+	// Links a VBO Attribute such as a position or color to the VAO
+
+	// Binds the VAO
+	void Bind();
+	// Unbinds the VAO
+	void Unbind();
+	// Deletes the VAO
+	void Delete();
+};
+
+#endif
